4b3a5091dad50a6d29a2d98591aee0d8eed37bd3
Daniel Suo <danielsuo@gmail.com>
2017-08-20 11:40:38 -0400

    Fixes #116. Install cmake via apt and brew rather than from source. Apologies for the excessive reformatting; if this bothers people or conflicts with other autoformatters (I'm using vim-autoformat defaults), I'll retract and leave whitespace alone.

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,79 +3,73 @@
 set -euo pipefail
 
 install_misc() {
-    # These dependencies are needed by a couple of different projects including
-    # redis, protobuf, grpc, etc.
-    sudo apt-get install \
-      ant \
-      autoconf \
-      automake \
-      curl \
-      g++ \
-      libcurl4-openssl-dev \
-      libev-dev\
-      libhiredis-dev \
-      libssl-dev \
-      libtool \
-      libuv-dev \
-      make \
-      unzip \
-      zlib1g-dev \
+  # These dependencies are needed by a couple of different projects including
+  # redis, protobuf, grpc, etc.
+  sudo apt-get install \
+    ant \
+    autoconf \
+    automake \
+    curl \
+    g++ \
+    libcurl4-openssl-dev \
+    libev-dev\
+    libhiredis-dev \
+    libssl-dev \
+    libtool \
+    libuv-dev \
+    make \
+    unzip \
+    zlib1g-dev \
+    cmake
 
 }
 
 install_clang() {
-    sudo bash -c "echo '' >> /etc/apt/sources.list"
-    sudo bash -c "echo '# http://apt.llvm.org/' >> /etc/apt/sources.list"
-    sudo bash -c "echo 'deb http://apt.llvm.org/trusty/ llvm-toolchain-trusty-4.0 main' >> /etc/apt/sources.list"
-    sudo bash -c "echo 'deb-src http://apt.llvm.org/trusty/ llvm-toolchain-trusty-4.0 main' >> /etc/apt/sources.list"
-    wget -O - http://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
-    sudo apt-get -y update
+  sudo bash -c "echo '' >> /etc/apt/sources.list"
+  sudo bash -c "echo '# http://apt.llvm.org/' >> /etc/apt/sources.list"
+  sudo bash -c "echo 'deb http://apt.llvm.org/trusty/ llvm-toolchain-trusty-4.0 main' >> /etc/apt/sources.list"
+  sudo bash -c "echo 'deb-src http://apt.llvm.org/trusty/ llvm-toolchain-trusty-4.0 main' >> /etc/apt/sources.list"
+  wget -O - http://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
+  sudo apt-get -y update
 
-    sudo apt-get install -y \
-        clang-4.0 clang-format-4.0 clang-tidy-4.0 lldb-4.0
-    sudo ln -s "$(which clang-4.0)" /usr/bin/clang
-    sudo ln -s "$(which clang++-4.0)" /usr/bin/clang++
-    sudo ln -s "$(which clang-format-4.0)" /usr/bin/clang-format
-    sudo ln -s "$(which clang-tidy-4.0)" /usr/bin/clang-tidy
-    sudo ln -s "$(which lldb-4.0)" /usr/bin/lldb
-    sudo ln -s "$(which lldb-server-4.0)" /usr/bin/lldb-server
+  sudo apt-get install -y \
+    clang-4.0 clang-format-4.0 clang-tidy-4.0 lldb-4.0
+  sudo ln -s "$(which clang-4.0)" /usr/bin/clang
+  sudo ln -s "$(which clang++-4.0)" /usr/bin/clang++
+  sudo ln -s "$(which clang-format-4.0)" /usr/bin/clang-format
+  sudo ln -s "$(which clang-tidy-4.0)" /usr/bin/clang-tidy
+  sudo ln -s "$(which lldb-4.0)" /usr/bin/lldb
+  sudo ln -s "$(which lldb-server-4.0)" /usr/bin/lldb-server
 }
 
 install_gpp() {
-    sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-    sudo apt-get update
-    sudo apt-get install -y g++-6
-    sudo ln -sf "$(which /usr/bin/g++-6)" /usr/bin/g++
-    sudo ln -sf "$(which /usr/bin/gcc-6)" /usr/bin/gcc
-}
-
-install_cmake() {
-    wget 'https://cmake.org/files/v3.6/cmake-3.6.2-Linux-x86_64.sh'
-    sh cmake-3.6.2-Linux-x86_64.sh --skip-license
-    echo 'export PATH="$PATH:$HOME/cmake-3.6.2-Linux-x86_64/bin"' >> ~/.bash_path
+  sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+  sudo apt-get update
+  sudo apt-get install -y g++-6
+  sudo ln -sf "$(which /usr/bin/g++-6)" /usr/bin/g++
+  sudo ln -sf "$(which /usr/bin/gcc-6)" /usr/bin/gcc
 }
 
 install_boost() {
-    sudo apt-get install libboost-dev
+  sudo apt-get install libboost-dev
 }
 
 install_postgres() {
-    sudo bash -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
-    wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-    sudo apt-get update
-    sudo apt-get install -y postgresql-9.6 postgresql-server-dev-9.6 python-dev
+  sudo bash -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+  wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+  sudo apt-get update
+  sudo apt-get install -y postgresql-9.6 postgresql-server-dev-9.6 python-dev
 }
 
 main() {
-    set -x
-    sudo apt-get -y update
-    install_misc
-    install_clang
-    install_gpp
-    install_cmake
-    install_boost
-    install_postgres
-    set +x
+  set -x
+  sudo apt-get -y update
+  install_misc
+  install_clang
+  install_gpp
+  install_boost
+  install_postgres
+  set +x
 }
 
 main
--- scripts/install_dependencies_osx.sh
+++ scripts/install_dependencies_osx.sh
@@ -3,19 +3,20 @@
 set -euo pipefail
 
 install_gtools() {
-	brew install gflags glog protobuf
+  brew install gflags glog protobuf
 }
 main() {
-    set -x
-    brew update
-    brew install boost
-    brew install zmq
-    brew install cereal
-    brew install redis
-    brew install libev
-    brew install hiredis
-    install_gtools
-    set +x
+  set -x
+  brew update
+  brew install boost
+  brew install zmq
+  brew install cereal
+  brew install redis
+  brew install libev
+  brew install hiredis
+  brew install cmake
+  install_gtools
+  set +x
 }
 
 main
